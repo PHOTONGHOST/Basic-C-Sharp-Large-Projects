fb220283e9746bc6087e769eb67aaab370d1812e
PHOTONGHOST <calebcbeckwith97@gmail.com>
2026-01-14 14:42:43 -0500

    Method Class Assignment
    This is a simple assignment for practicing  void methods and named parameters.

---
+++ MethodClassAssignment/MathDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    // This class contains a void method that performs both a math operation and displays values to the console
+    public class MathDisplay
+    {
+        // This void method takes two integers as parameters
+        public void DoMath(int firstNumber, int secondNumber)
+        {
+            // Performs a math operation on the first integer
+            int result = firstNumber * 2;
+
+            // Then displays the result of the math operation
+            Console.WriteLine("Result of math operation on first number: " + result);
+
+            // This will display the second integer to the screen
+            Console.WriteLine("Second number passed in: " + secondNumber);
+        }
+    }
+}
---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Part 1: This instantiates the MathDisplay class
+            MathDisplay mathDisplay = new MathDisplay();
+
+            // Part 2: This calls the method using positional arguments
+            mathDisplay.DoMath(5, 10);
+
+            // Part 3: This calls the method using the named arguments
+            mathDisplay.DoMath(firstNumber: 7, secondNumber: 20);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
